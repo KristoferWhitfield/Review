07d6df974dd2adda10ff433b2b5eece70459a9b8
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-03-17 22:54:05 -0400

    Corrected

---
+++ variable-fun-time/js/main.js
@@ -0,0 +1,35 @@
+//--- Easy
+//create a variable and assign it a number
+
+let num = 5
+
+//minus 10 from that number
+
+num -= 10
+
+//print that number to the console
+console.log(num)
+//--- Medium
+//create a variable that holds a value from the input
+
+
+// queryselector makes the function happen when person hits click
+document.querySelector('h1').addEventListener('click',alertVal)
+
+function alertVal(){
+  let inputVal = Number(document.querySelector('input').value)
+  // number makes sure its a number
+
+
+      //add 25 to that number
+      inputVal += 25
+      //alert that number
+      alert(inputVal)
+}
+
+
+
+//--- Hard
+//create a variable that holds the h1
+
+//add an event listener to that element that console logs the sum of the two previous variables
